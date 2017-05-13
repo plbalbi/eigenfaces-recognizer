07b4978b3ab5d3a0fbb5cd001b93919a819ff0e6
npazosmendez <npazosmendez@gmail.com>
2017-05-13 19:08:00 -0300

    Empezando parser de entrada

---
+++ src/parser.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Tipos
+struct test{
+    // path a la imagen de la cara a identificar
+    string path;
+
+    // nro de sujeto al que pertenece la cara
+    int respuesta;
+};
+
+// un sujeto es una lista de las imágenes (paths) que lo identifican
+typedef vector<string> sujeto ;
+
+
+// Declaraciones
+void read_input(string input_path, unsigned int &img_ancho, unsigned int &img_alto,
+                unsigned int &k, vector<sujeto> &sujetos, vector<test> &tests);
