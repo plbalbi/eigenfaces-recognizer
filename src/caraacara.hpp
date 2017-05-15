248926d75b632ec6ab427398fa29cf4e3fd2f1c5
pab <pablo.l.balbi@gmail.com>
2017-05-14 21:51:11 -0300

    posible header

---
+++ src/caraacara.hpp
@@ -0,0 +1,23 @@
+#ifndef CARAACARA_H
+#define CARAACARA_H
+
+#include "PGMDozer.hpp"
+#include "parser.hpp"
+#include <iostream>
+//Definiendo includes
+
+//MatrixXf X(img_por_sujeto*sujetos.size(), img_alto*img_ancho);
+//RowVectorXf media(1, img_alto*img_ancho);
+
+// -------------- separador de bajo presupuesto --------------
+// Reduccion de espacio
+int metodoPotencia(MatrixXf& B, RowVectorXf& v, int iteraciones);
+void deflacionar(MatrixXf& B, RowVectorXf& v, int lambda);
+
+// -------------- separador de bajo presupuesto --------------
+// Clasificacion
+int kNN();
+// inline porque solo sería multiplicar un vector
+void inline reducirEspacio(RowVectorXf& imagen, MatrixXf& TL);
+
+#endif
