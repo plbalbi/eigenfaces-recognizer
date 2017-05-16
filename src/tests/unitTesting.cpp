a186b1be08f6be7540e87cc33706777c058c88ab
DierLaptop <dario6reyes6@yahoo.com.ar>
2017-05-16 14:48:47 -0300

    Arregla identación

--- src/tests/unitTesting.cpp
+++ src/tests/unitTesting.cpp
@@ -7,19 +7,19 @@ void test_metodoPotencia(){
     int OK_count = 0;
     int test_qty = 100;
     for (int i = 0; i < test_qty; i++) {
-    std::cout << "TEST " << i << ":\t\t";
-    MatrixXf M = MatrixXf::Random(10,10);
-    VectorXf v = VectorXf::Random(10,1);
-    int l = metodoPotencia(M, v, 100);
-    VectorXf r = M*v - l*v;
-    // std::cout << '\n' << r.transpose() << '\n';
-    if (r.norm() < umbral) {
-        std::cout << termcolor::green << "OK" << termcolor::reset << '\n';
-        OK_count++;
-    }else{
-        std::cout << termcolor::red << "FALLA" << termcolor::reset << '\n';
+        std::cout << "TEST " << i << ":\t\t";
+        MatrixXf M = MatrixXf::Random(10,10);
+        VectorXf v = VectorXf::Random(10,1);
+        int l = metodoPotencia(M, v, 100);
+        VectorXf r = M*v - l*v;
+        // std::cout << '\n' << r.transpose() << '\n';
+        if (r.norm() < umbral) {
+            std::cout << termcolor::green << "OK" << termcolor::reset << '\n';
+            OK_count++;
+        }else{
+            std::cout << termcolor::red << "FALLA" << termcolor::reset << '\n';
+        }
     }
-  }
     std::cout << "-----------------------------------------" << '\n';
     std::cout << "Paso " << OK_count << " tests de " << test_qty << '\n';
 }
