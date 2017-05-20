1e1130c62d565d934d97e2ee381c28142e6887d7
npazosmendez <npazosmendez@gmail.com>
2017-05-20 00:39:16 -0300

    Implementado calculo de autovectores con X*Xt La versión anterior todavía está en transCaracteristica_v1(), y la nueva en transCaracteristica_v2(). Los nombres son poco descriptivos, pero no se me ocurrió nada todavía.

--- src/main.cpp
+++ src/main.cpp
@@ -46,8 +46,10 @@ int main(int argc, char const *argv[]) {
     cargar_datos(img_alto, img_ancho, sujetos, X, media);
     MatrixXd V;
     std::vector<double> autovalores;
-    // transfCaracteristica_v2(X, k, 500, V, autovalores);
-    transfCaracteristica_v1(X, k, 50, V, autovalores);
+
+    transfCaracteristica_v2(X, k, 500, V, autovalores); //calcula los autovectores con la matriz X*Xt
+    // transfCaracteristica_v1(X, k, 500, V, autovalores); //calcula los autovectores con la matriz Xt*X
+    
     MatrixXd Vt = V.transpose();
     std::cout << "Armando TL al espacio copado...\t\t" << termcolor::green << "OK" << termcolor::reset << std::endl;
 
